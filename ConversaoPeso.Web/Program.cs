f265e8f5ed7e89357db80768d268bf5d704e46d8
wwrmaia <wwrmaia@gmail.com>
2021-10-02 14:46:46 -0300

    Ajuste de código

--- ConversaoPeso.Web/Program.cs
+++ ConversaoPeso.Web/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace ConversaoPeso.Web
+{
     public class Program
     {
         public static void Main(string[] args)
